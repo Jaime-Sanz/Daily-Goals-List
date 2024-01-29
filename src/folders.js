414d3ba29a6103b40219361282283bf1023dc4c5
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2024-01-29 00:21:38 -0800

    added(folder function)
    allowed user to add or delete folder along with description on folder hover

--- src/folders.js
+++ src/folders.js
@@ -1,3 +1,5 @@
+import { showCreateScreen } from ".";
+
 export class Folder {
     constructor(name, description, priority) {
         this.name = name;
@@ -11,14 +13,86 @@ export class Folder {
         this.todos.push(newTodo);
         return newTodo;
     }
-
-    deleteTodoByTitle(title) {
-        const index = this.todos.findIndex(todo => todo.title === title);
-        if (index !== -1) {
-          const deletedTodo = this.todos.splice(index, 1)[0];
-          return deletedTodo;
-        } else {
-          return null; 
-        }
-    }
 }
+
+function displayFolder(folder) {
+    let folderHolder = document.getElementById('folderHolder');
+
+    let folderDescription = document.createElement('div');
+    let folderDescriptionText = document.createElement('p');
+
+    folderDescription.classList.add('hoverBubble');
+
+    folderDescription.appendChild(folderDescriptionText);
+    folderHolder.appendChild(folderDescription);
+
+    folderDescriptionText.textContent = folder.description;
+  
+    // Create elements
+    let folderContainer = document.createElement('div');
+    let folderIcon = document.createElement('span');
+    let folderName = document.createElement('a');
+    let folderPlus = document.createElement('a');
+    let folderDelete = document.createElement('a');
+  
+    // Set content and attributes
+    folderName.textContent = folder.name;
+    folderIcon.textContent = 'folder';
+    folderPlus.textContent = 'add';
+    folderDelete.textContent = 'delete';
+    folderName.href = '#';
+    folderPlus.href = '#';
+    folderDelete.href = '#';
+    folderPlus.classList.add('material-symbols-outlined');
+    folderIcon.classList.add('material-symbols-outlined');
+    folderDelete.classList.add('material-symbols-outlined');
+  
+    // Apply color based on priority
+    if (folder.priority === 'High') {
+      folderIcon.style.color = 'red';
+    } else if (folder.priority === 'Normal') {
+      folderIcon.style.color = 'green';
+    } else {
+      folderIcon.style.color = 'blue';
+    }
+
+    folderName.addEventListener('mouseover', function() {
+        folderDescription.style.visibility = 'visible';
+    });
+
+    folderName.addEventListener('mouseout', function() {
+        folderDescription.style.visibility = 'hidden';
+    });
+
+    folderDelete.addEventListener('click', function() {
+        let text = "Are You Sure You Want to Delete?";
+        if (confirm(text) == true) {
+            folderHolder.removeChild(folderContainer);
+        }
+      });
+
+    folderPlus.addEventListener('click', function() {
+        
+    });
+  
+    // Append elements
+    folderHolder.appendChild(folderContainer);
+    folderContainer.appendChild(folderIcon);
+    folderContainer.appendChild(folderName);
+    folderContainer.appendChild(folderPlus);
+    folderContainer.appendChild(folderDelete);
+
+    showCreateScreen();
+  }
+  
+
+document.getElementById("create-folder").addEventListener("submit", function(event){
+    let folderName = document.getElementById("folder-name").value;
+    let folderDescription = document.getElementById("folder-description").value;
+    let folderPriority = document.getElementById("folderPrio").value;
+    let folderForm = document.getElementById("create-folder");
+
+    const myFolder = new Folder(folderName, folderDescription, folderPriority);
+    displayFolder(myFolder);
+    folderForm.reset();
+    event.preventDefault();});;
--- src/index.js
+++ src/index.js
@@ -28,3 +28,4 @@ export function showCreateScreen() {
        }
 }
 //End of Exported Functions
+
